09f9ff0d61fdcc7606ba680035d6dabc41a18e80
Ryan Mitchell <44037867+ryanjmitchell@users.noreply.github.com>
2019-02-28 10:10:53 -0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,36 +1,52 @@
 window.onload = function(){
 alert('If you give it permission, this web page will access to your location in order to demonstrate how device sensors can interact with web maps.');
-} // On load, this alert notifies the user that the page will ask to access their location and gives a reason why. You can easily modify this text.
+}
 
-var map = L.map('map').fitWorld(); // Initialize the map
+var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicnlhbmptaXRjaCIsImEiOiJjamhhdDBjaXgwcmZlMzBxZ2t1cnZ4bnFnIn0.4tXv0Yvk06rDbYp7ZLSdAw';
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  maxZoom: 18,
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  id: 'mapbox.light', // changed to mapbox.light
-  accessToken: 'pk.eyJ1IjoicnlhbmptaXRjaCIsImEiOiJjamhhdDBjaXgwcmZlMzBxZ2t1cnZ4bnFnIn0.4tXv0Yvk06rDbYp7ZLSdAw' // This is my token
-}).addTo(map);
+var light   = L.tileLayer(mbUrl, {id: 'mapbox.light', maxZoom:18, attribution: mbAttr}),
+	dark  = L.tileLayer(mbUrl, {id: 'mapbox.dark', maxZoom:18, attribution: mbAttr});
 
-//the below JS code takes advantage of the Geolocate API as it is incorporated in the Leaflet JS API with the locate method
-function onLocationFound(e) { //this function does three things if the location is found: it defines a radius variable, adds a popup to the map, and adds a circle to the map.
+var map = L.map('map', {
+	layers:[light]}).fitWorld(); 
+var baseLayers = {
+		"light": light,
+		"dark": dark
+	};
+L.control.layers(baseLayers).addTo(map);
 
-  var radius = e.accuracy / 2; //this defines a variable radius as the accuracy value returned by the locate method divided by 2. It is divided by 2 because the accuracy value is the sum of the estimated accuracy of the latitude plus the estimated accuracy of the longitude. The unit is meters.
+var currentTime = new Date().getHours()
+
+if (7 <= currentTime && currentTime < 19){
+	map.removeLayer(dark);
+	map.addLayer(light);
+}
+else{
+	map.removeLayer(light);
+	map.addLayer(dark);
+}
+
+
+function onLocationFound(e) {
+
+  var radius = e.accuracy / 2;
+  
+  var latlong = e.latlng
 
   L.marker(e.latlng).addTo(map)
-    .bindPopup("You are within " + radius + "m of this point.").openPopup();
-  //this adds a Leaflet popup to the map at the lat and long returned by the locate function. The text of the popup is defined here as well. Please change this text to specify what unit the radius is reported in.
+    .bindPopup("You are within " + radius + "m of this point.<br>" + latlong).openPopup();
 
   L.circle(e.latlng, radius).addTo(map); // this adds a Leaflet circle to the map at the lat and long returned by the locate function. Its radius is set to the var radius defined above.
 
-  /*if (radius < 30) {
-      L.circle(e.latlng, radius, {color: 'green'}.addTo(map);
+  if (radius < 30) {
+      L.circle(e.latlng, radius, {color: 'green'}).addTo(map);
   }
   else{
-      L.circle(e.latlng, radius, {color: 'red'}).addTo(map),
-  } */
-  //this adds a Leaflet circle to the map at the lat and long returned by the locate function. Its radius is set to the var radius defined above. If the radius is less than 30, the color of the circle is blue. If it is more than 30, the color is red. Comment out the line of code that adds the simple circle and uncomment the seven lines of code that enable the responsively colored circle. NOTE: there are two syntax errors in the code that you must correct in order for it to function.
+      L.circle(e.latlng, radius, {color: 'red'}).addTo(map);
+  }
 }
 
 function onLocationError(e) {
